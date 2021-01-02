120e2c50af768927e9c9d8c9f1be3f8b3081fb86
vpoat <vpoat@pop-os.localdomain>
2021-01-02 12:37:38 +0700

    up

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -244,8 +244,6 @@ export GOPATH=$(go env GOPATH)
 #autoload -U promptinit; promptinit
 #prompt pure
 
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 source ~/powerlevel10k/powerlevel10k.zsh-theme
