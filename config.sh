10499463708b08e861d45863bd8e19a6f2a01cc8
vpoat <vpoat@pop-os.localdomain>
2021-02-28 14:12:27 +0700

    up

--- config.sh
+++ config.sh
@@ -162,3 +162,29 @@ mkdir /tmp/LS_COLORS && curl -L https://api.github.com/repos/vpeopleonatank/LS_C
 
 echo "turn off tmux auto restore"
 touch ~/tmux_no_auto_restore
+
+function install_fira_code() {
+  #!/usr/bin/env bash
+
+  fonts_dir="${HOME}/.local/share/fonts"
+  if [ ! -d "${fonts_dir}" ]; then
+      echo "mkdir -p $fonts_dir"
+      mkdir -p "${fonts_dir}"
+  else
+      echo "Found fonts dir $fonts_dir"
+  fi
+
+  for type in Bold Light Medium Regular Retina; do
+      file_path="${HOME}/.local/share/fonts/FiraCode-${type}.ttf"
+      file_url="https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true"
+      if [ ! -e "${file_path}" ]; then
+          echo "wget -O $file_path $file_url"
+          wget -O "${file_path}" "${file_url}"
+      else
+    echo "Found existing file $file_path"
+      fi;
+  done
+
+  echo "fc-cache -f"
+  sudo fc-cache -f
+}
