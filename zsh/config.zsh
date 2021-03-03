7627c765257a2c3cb0abb392400bb4d455f448d7
vpoat <vpoat@pop-os.localdomain>
2021-03-03 20:09:50 +0700

    up

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -176,7 +176,8 @@ alias countdown='~/git/countdown/countdown'
 alias cd_basic_algo_codelearn='cd /mnt/vpoat/Data/Code/algo_merge/contest/codelearn/basic_algo'
 alias psudo='sudo env PATH="$PATH"'
 alias lzd='sudo lazydocker'
-alias vno='HOME=$(mktemp -d) nvim -u NONE -U NONE -N -i NONE -u $HOME/.dotfiles/tool/vim/.vimrc_server'
+# alias vno='HOME=$(mktemp -d) vim -u NONE -U NONE -N -i NONE -u $HOME/.dotfiles/tool/vim/.vimrc_server'
+alias vno='vim -u $HOME/.dotfiles/tool/vim/.vimrc_server -U NONE -N -i NONE'
 fg() {
   git add .
   git commit -m "update"
