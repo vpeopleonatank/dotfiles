553ca871351b7c9f5ef35be611e7d4355cc3dcfa
vpeopleonatank <vpeopleonatank@gmail.com>
2023-01-02 11:46:20 +0700

    update nvm env

--- zsh/zenvs.zsh
+++ zsh/zenvs.zsh
@@ -61,3 +61,6 @@ eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 export ANDROID_SDK_ROOT=$HOME/Android/Sdk
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
