38b3a46b7f6468803b37e1ee94d284dc8fe67b15
vpeopleonatank <vpeopleonatank@gmail.com>
2023-01-02 16:12:17 +0700

    remove

--- install.sh
+++ install.sh
@@ -35,7 +35,6 @@ function install_kitty() {
   curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
   ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
   cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
-  ~/.local/share/applications/kitty.desktop
 }
 
 function install_bat() {
