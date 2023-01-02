69153b118e3cde7fb748262b4e37b76d4934ff50
vpeopleonatank <vpeopleonatank@gmail.com>
2023-01-02 17:01:41 +0700

    change fd

--- zsh/zenvs.zsh
+++ zsh/zenvs.zsh
@@ -50,7 +50,7 @@ fi
 
 export PATH=~/.npm-global/bin:$PATH
 
-export FZF_DEFAULT_COMMAND='fdfind'
+export FZF_DEFAULT_COMMAND='fd'
 
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
