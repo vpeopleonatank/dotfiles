6b3b920fe29b64b2f76ef8f639fff5d73abeb6fe
vpeopleonatank <vpeopleonatank@gmail.com>
2023-11-16 19:57:07 +0700

    add lazyvim

--- config.sh
+++ config.sh
@@ -2,15 +2,13 @@
 
 # Symlink Kitty terminal config
 if [ ! -d $HOME/.config/kitty ]; then
-  mkdir $HOME/.config/kitty
+	mkdir $HOME/.config/kitty
 else
-  echo "kitty config dir existed"
+	echo "kitty config dir existed"
 fi
-if [ -f $HOME/.config/kitty/kitty.conf ]
-then
-  ln -s ~/.dotfiles/tool/kitty/kitty.conf ~/.config/kitty/kitty.conf
+if [ -f $HOME/.config/kitty/kitty.conf ]; then
+	ln -s ~/.dotfiles/tool/kitty/kitty.conf ~/.config/kitty/kitty.conf
 fi
-
 
 echo "turn off tmux auto restore"
 touch ~/tmux_no_auto_restore
@@ -18,7 +16,7 @@ touch ~/tmux_no_auto_restore
 # Symlink config
 echo "Symlink nvim config"
 if [ ! -d $HOME/.config/nvim ]; then
-  mkdir $HOME/.config/nvim
+	mkdir $HOME/.config/nvim
 fi
 sudo ln -s $HOME/.dotfiles/tool/vim/init.vim $HOME/.config/nvim/init.vim
 sudo ln -s $HOME/.dotfiles/tool/vim/coc-settings-nvim.json $HOME/.config/nvim/coc-settings.json
@@ -30,17 +28,18 @@ nvim +"CocInstall coc-explorer coc-json coc-tsserver" +qall
 
 # config tmux
 if [ ! -d $HOME/.tmux/plugins/tpm ]; then
-    echo "install zsh-z"
+	echo "install zsh-z"
 
-  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+	git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 else
-    echo "tpm is installed"
+	echo "tpm is installed"
 fi
 echo 'source ~/.dotfiles/tool/tmux/config.tmux' >$HOME/.tmux.conf
 
 echo "Symlink Tmux configuration successfully ^~^"
-if [ ! -d $HOME/.config/jesseduffield/lazygit ];
-then
-  mkdir -p $HOME/.config/jesseduffield/lazygit
+if [ ! -d $HOME/.config/jesseduffield/lazygit ]; then
+	mkdir -p $HOME/.config/jesseduffield/lazygit
 fi
 sudo ln -s $HOME/.dotfiles/tool/lazygit/config.yml $HOME/.config/jesseduffield/lazygit/config.yml
+
+sudo ln -s /home/vpoat/.dotfiles/tool/nvim /home/vpoat/.config/nvim
